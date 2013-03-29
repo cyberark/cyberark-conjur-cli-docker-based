7616b197b7382d664300fadb8859cfad5f2d812e
Kevin Gilpin <kgilpin@gmail.com>
2013-03-29 14:33:32 -0400

    added asset:create and asset:exists

--- lib/conjur/command/assets.rb
+++ lib/conjur/command/assets.rb
@@ -3,6 +3,29 @@ require 'conjur/command'
 
 class Conjur::Command::Assets < Conjur::Command
   self.prefix = :asset
+
+  desc "Create an asset"
+  arg_name "kind id"
+  command :create do |c|
+    acting_as_option(c)
+    
+    c.action do |global_options, options, args|
+      kind = require_arg(args, 'kind')
+      
+      m = "create_#{kind}"
+      record = if api.method(m).arity == 1
+        id = args.shift
+        if id
+          options[:id] = id
+        end
+        api.send(m, options)
+      else
+        id = require_arg(args, 'id')
+        api.send(m, id, options)
+      end
+      display(record, options)
+    end
+  end
   
   desc "Show an asset"
   arg_name "kind id"
@@ -11,6 +34,16 @@ class Conjur::Command::Assets < Conjur::Command
       kind = require_arg(args, "kind")
       id = require_arg(args, "resource-id")
       display api.send(kind, id).attributes
+    end
+  end
+
+  desc "Checks for the existance of an asset"
+  arg_name "kind id"
+  command :exists do |c|
+    c.action do |global_options,options,args|
+      kind = require_arg(args, "kind")
+      id = require_arg(args, "id")
+      puts api.send(kind, id).exists?
     end
   end
 
