9d309208744e96070f6cf02fcad078f532606e67
Rafał Rzepecki <divided.mind@gmail.com>
2013-05-05 19:00:32 +0200

    Spec refactoring.

--- spec/command/authn_spec.rb
+++ spec/command/authn_spec.rb
@@ -3,13 +3,23 @@ require 'tempfile'
 
 describe Conjur::Command::Authn do
   let(:netrcfile) { Tempfile.new 'authtest' }
+  let(:netrc) { Netrc.read(netrcfile.path) }
+  let(:host) { 'https://authn.example.com' }
+
   before do
-    Conjur::Authn.stub netrc: Netrc.read(netrcfile.path)
+    Conjur::Authn.stub netrc: netrc, host: host
   end
-  describe_command 'authn:logout' do
-    it "deletes credentials" do
-      Conjur::Authn.should_receive :delete_credentials
-      invoke
+
+  context "when logged in" do
+    let(:username) { 'dknuth' }
+    let(:api_key) { 'sekrit' }
+    before { netrc[host] = [username, api_key] }
+
+    describe_command 'authn:logout' do
+      it "deletes credentials" do
+        invoke
+        netrc[host].should_not be
+      end
     end
   end
 end
