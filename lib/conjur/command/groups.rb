dab7d1500373c2c1d2a4655c01f4d67dc8b56c2a
Rafał Rzepecki <divided.mind@gmail.com>
2013-06-29 14:22:42 +0200

    Only grant (or revoke) admin option in group:members:add if requested. Fixes BC#49083893-high-unable-to-add-a

--- lib/conjur/command/groups.rb
+++ lib/conjur/command/groups.rb
@@ -20,17 +20,31 @@ class Conjur::Command::Groups < Conjur::Command
   desc "Add a new group member"
   arg_name "group member"
   command :"members:add" do |c|
-    c.desc "Grant with admin option"
+    c.desc "Also grant the admin option"
     c.switch [:a, :admin]
+
+    # perhaps this belongs to member:remove, but then either
+    # it would be possible to grant membership with member:revoke,
+    # or we would need two round-trips to authz
+    c.desc "Revoke the grant option if it's granted"
+    c.switch [:r, :'revoke-admin']
     
     c.action do |global_options,options,args|
       group = require_arg(args, 'group')
       member = require_arg(args, 'member')
       
       group = api.group(group)
-      api.role(group.roleid).grant_to member, !!options[:admin]
-
-      puts "Membership granted"
+      opts = nil
+      message = "Membership granted"
+      if options[:admin] then
+        opts = { admin_option: true }
+        message = "Adminship granted"
+      elsif options[:'revoke-admin'] then
+        opts = { admin_option: false }
+        message = "Adminship revoked"
+      end
+      api.role(group.roleid).grant_to member, opts
+      puts message
     end
   end
 
