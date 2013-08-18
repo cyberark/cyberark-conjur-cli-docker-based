819661af67354799b029bb14b86adfa9455b2e16
Hleb <g.rubanau@gmail.com>
2013-08-18 13:17:22 +0300

    added rspec for id conversion functions (+refactored them)

--- lib/conjur/command.rb
+++ lib/conjur/command.rb
@@ -21,6 +21,7 @@ module Conjur
         @@api ||= Conjur::Authn.connect
       end
 
+      # injects account into 2-tokens id
       def full_resource_id id
         parts = id.split(':') unless id.nil? 
         if id.blank? or parts.size < 2
@@ -32,23 +33,14 @@ module Conjur
         id
       end
 
-      def parse_full_resource_id id
-        id_parts = id.split(':')
-        account = id_parts[0]
-        kind = id_parts[1]
-        resource_id = id_parts[2,id_parts.size].join(":")
-        resource_id = nil if resource_id.blank? 
-        [account, kind, resource_id]
-      end
-      
+      # removes accounts from 3+-tokens id, extracts kind
       def get_kind_and_id_from_args args, argname='id'
-        _, kind, id = parse_full_resource_id( 
-                        full_resource_id(
-                          require_arg(args, argname)
-                        )
-                      )
-        kind.gsub!('-', '_')
-        [kind, id]
+        flat_id = require_arg(args, argname)
+        tokens=flat_id.split(':')
+        raise "At least 2 tokens expected in #{flat_id}" if tokens.size<2
+        tokens.shift if tokens.size>=3 # get rid of account
+        kind=tokens.shift.gsub('-','_')
+        [kind, tokens.join(':')]
       end
 
       def conjur_account
