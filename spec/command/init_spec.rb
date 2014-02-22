8ee63ebb7854b7a9f22d580600f543495dc0c226
Kevin Gilpin <kgilpin@gmail.com>
2014-02-22 13:08:37 -0500

    add command to create .conjurrc and write certificate to a local file

---
+++ spec/command/init_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+tmpdir = Dir.mktmpdir
+
+describe Conjur::Command::Init do
+  context logged_in: false do
+    before {
+      File.stub(:exists?).and_return false
+    }
+    describe_command 'init -a the-account' do
+      it "writes config file" do
+        # Stub hostname and cert name
+        HighLine.any_instance.stub(:ask).and_return ""
+        HighLine.any_instance.stub(:ask).and_return []
+        File.should_receive(:open)
+        invoke
+      end
+    end
+    describe_command 'init -a the-account -h localhost -c the-cert' do
+      it "writes config and cert files" do
+        File.should_receive(:open).twice
+        invoke
+      end
+    end
+    context "in a temp dir" do
+      describe_command "init -f #{tmpdir}/.conjurrc -a the-account -h localhost -c the-cert" do
+        it "writes config and cert files" do
+          invoke
+          
+          File.read(File.join(tmpdir, ".conjurrc")).should == """---
+account: the-account
+plugins:
+- environment
+- layer
+- key-pair
+appliance_url: https://localhost
+cert_file: #{tmpdir}/conjur-the-account.pem
+"""
+          File.read(File.join(tmpdir, "conjur-the-account.pem")).should == "the-cert\n"
+        end
+      end
+    end
+  end
+end
