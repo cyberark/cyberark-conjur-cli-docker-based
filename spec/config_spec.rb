40ab32e5e8c769c96c1f9271d0829f0298c7e743
Rafał Rzepecki <divided.mind@gmail.com>
2014-07-07 19:53:28 +0200

    Spec envar-rc behavior

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -26,19 +26,33 @@ describe Conjur::Config do
     end
   end
 
+  let(:load!) { Conjur::Config.load([ File.expand_path('conjurrc', File.dirname(__FILE__)) ]) }
+  let(:cert_path) { File.expand_path('conjur-ci.pem', File.dirname(__FILE__)) }
+
   describe "#load" do
     it "resolves the cert_file" do
-      Conjur::Config.load([ File.expand_path('conjurrc', File.dirname(__FILE__)) ])
+      load!
       
-      Conjur::Config[:cert_file].should == File.expand_path('conjur-ci.pem', File.dirname(__FILE__))
+      Conjur::Config[:cert_file].should == cert_path
     end
   end
   describe "#apply" do
+    before { OpenSSL::SSL::SSLContext::DEFAULT_CERT_STORE.stub(:add_file) }
+
     let(:cert_file) { "/path/to/cert.pem" }
     it "trusts the cert_file" do
       Conjur::Config.class_variable_set("@@attributes", { 'cert_file' => cert_file })
       OpenSSL::SSL::SSLContext::DEFAULT_CERT_STORE.should_receive(:add_file).with cert_file  
       Conjur::Config.apply
+    end
+
+    # is it something we want? I just spec it here to document it -div
+    it "shadows envars with rc" do
+      url = 'https://other-conjur.example.com'
+      ENV['CONJUR_APPLIANCE_URL'] = url
+      load!
+      Conjur::Config.apply
+      expect(Conjur.configuration.appliance_url).to_not eq url
     end
   end
 end
