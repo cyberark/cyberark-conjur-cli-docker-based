d49f2fdf89054d10a36c0605b7286410b4416266
Rafał Rzepecki <divided.mind@gmail.com>
2014-12-21 03:52:17 +0100

    Refactor authn_spec a bit

--- spec/authn_spec.rb
+++ spec/authn_spec.rb
@@ -2,31 +2,38 @@ require 'conjur/authn'
 require 'conjur/config'
 
 describe Conjur::Authn do
+  let(:netrc) { Netrc.read '' }
+  before do
+    Conjur::Authn.instance_variable_set("@netrc", netrc)
+  end
+
   describe "credentials from environment" do
-    before {
+    before do
       Conjur::Authn.instance_variable_set("@credentials", nil)
       expect(ENV).to receive(:[]).with("CONJUR_AUTHN_LOGIN").and_return "the-login"
       expect(ENV).to receive(:[]).with("CONJUR_AUTHN_API_KEY").and_return "the-api-key"
-    }
-    after {
+    end
+
+    after do
       Conjur::Authn.instance_variable_set("@credentials", nil)
-    }
+    end
+
     it "are used to authn" do
       expect(Conjur::Authn.get_credentials).to eq([ "the-login", "the-api-key" ])
     end
+
     it "are not written to netrc" do
-      allow(Conjur::Authn).to receive(:write_credentials).and_raise "should not write credentials"
+      expect(Conjur::Authn).not_to receive(:write_credentials)
       Conjur::Authn.get_credentials
     end
   end
+
   describe "netrc" do
-    before {
-      Conjur::Authn.instance_variable_set("@netrc", nil)
-      expect(Conjur::Config).to receive(:[]).with(:netrc_path).and_return path
-    }
-    after {
-      Conjur::Authn.instance_variable_set("@netrc", nil)
-    }
+    let(:netrc) { nil }
+    before do
+      allow(Conjur::Config).to receive(:[]).with(:netrc_path).and_return path
+    end
+
     context "with specified netrc_path" do
       let(:path) { double("path") }
       it "consults Conjur::Config for netrc_path" do
@@ -34,6 +41,7 @@ describe Conjur::Authn do
         expect(Conjur::Authn.netrc).to eq(netrc)
       end
     end
+
     context "without specified netrc_path" do
       let(:path) { nil }
       it "uses default netrc path" do
