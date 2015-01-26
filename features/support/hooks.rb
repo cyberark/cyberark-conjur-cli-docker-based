f7398b67fe0ff0905de119e9ad9ea4da8e8348b9
Jon Mason <jonathan.j.mason@gmail.com>
2015-01-26 14:43:32 -0600

    crank up aruba subprocess timeout

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,17 +2,17 @@ require 'ostruct'
 
 class MockAPI
   attr_reader :things
-  
+
   def initialize
     @things = {}
   end
-  
+
   def thing(kind, id)
-    (@things[kind.to_sym] || []).find{|r| r.id == id}  
+    (@things[kind.to_sym] || []).find{|r| r.id == id}
   end
-  
+
   def thing_like(kind, id_pattern)
-    (@things[kind.to_sym] || []).find{|r| id_pattern.match(r.id)}  
+    (@things[kind.to_sym] || []).find{|r| id_pattern.match(r.id)}
   end
 
   def create_host(options = {})
@@ -24,56 +24,56 @@ class MockAPI
     end
     host ||= create_thing(:host, id, options, role: true, api_key: true)
   end
-  
+
   def create_user(id, options = {})
     thing(:user, id) || create_thing(:user, id, options, role: true, api_key: true)
   end
-  
+
   def create_variable(mime_type, kind)
     create_thing(:user, SecureRandom.uuid, mime_type: mime_type, kind: kind)
   end
-  
+
   def create_resource(id, options = {})
     resource(id).tap do |resource|
       resource.send(:"exists?=", true)
       populate_options resource, options
     end
   end
-  
+
   def create_role(id, options = {})
     role(id).tap do |role|
       role.send(:"exists?=", true)
       populate_options role, options
     end
   end
-  
+
   [ :user, :host ].each do |kind|
     define_method kind do |id|
       thing(kind, id)
     end
   end
-  
+
   def role(id)
     raise "Role id must be a string" unless id.is_a?(String)
     thing(:role, id) || create_thing(:role, id, { exists?: false }, role: true)
   end
-  
+
   def resource(id)
     raise "Resource id must be a string" unless id.is_a?(String)
     thing(:resource, id) || create_thing(:resource, id, exists?: false)
   end
-  
+
   protected
-  
+
   def create_thing(kind, id, options, kind_options = {})
     thing = OpenStruct.new(kind: kind, id: id, exists?: true)
-    
+
     class << thing
       def permit(privilege, role, options = {})
         (self.permissions ||= []) << OpenStruct.new(privilege: privilege, role: role.id, grant_option: !!options[:grant_option])
       end
     end
-    
+
     if kind_options[:api_key]
       thing.api_key = SecureRandom.uuid
     end
@@ -85,20 +85,20 @@ class MockAPI
         end
       end
     end
-    
+
     populate_options(thing, options)
-    
+
     store_thing kind, thing
-    
+
     thing
   end
-  
+
   def populate_options(thing, options)
     options.each do |k,v|
       thing.send("#{k}=", v)
     end
   end
-  
+
   def store_thing(kind, thing)
     (things[kind] ||= []) << thing
   end
@@ -107,11 +107,11 @@ end
 Before("@dsl") do
   puts "Using MockAPI"
   puts "Using account 'cucumber'"
-  
+
   require 'conjur/api'
   require 'conjur/config'
   require 'conjur/dsl/runner'
-  
+
   Conjur.stub(:env).and_return "ci"
   Conjur.stub(:stack).and_return "ci"
   Conjur.stub(:account).and_return "cucumber"
@@ -128,4 +128,5 @@ Before('@real-api') do
   puts "contents = #{File.read(cfg)}"
   Conjur::Config.load([cfg])
   Conjur::Config.apply
+  @aruba_timeout_seconds = 15
 end
