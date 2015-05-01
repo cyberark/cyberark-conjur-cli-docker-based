d55f70e7860098db2b2051c594f760e5cec4b3bd
Kevin Gilpin <kgilpin@gmail.com>
2015-05-01 10:51:28 -0400

    [delivers #92536976] don't check netrc world-readability on Windows

--- spec/authn_spec.rb
+++ spec/authn_spec.rb
@@ -27,26 +27,54 @@ describe Conjur::Authn do
       Conjur::Authn.get_credentials
     end
   end
-
+  
   describe "netrc" do
-    let(:netrc) { nil }
-    before do
-      allow(Conjur::Config).to receive(:[]).with(:netrc_path).and_return path
-    end
-
-    context "with specified netrc_path" do
-      let(:path) { "/a/dummy/netrc/path" }
-      it "consults Conjur::Config for netrc_path" do
-        expect(Netrc).to receive(:read).with(path).and_return netrc = double("netrc")
-        expect(Conjur::Authn.netrc).to eq(netrc)
+    describe "fail_if_world_readable" do
+      let(:path) { "the-path" }
+      around { |example|
+        host_os = RbConfig::CONFIG["host_os"]
+        RbConfig::CONFIG["host_os"] = os
+        begin
+          example.run
+        ensure 
+          RbConfig::CONFIG["host_os"] = host_os
+        end
+      }
+      context "on Windows" do
+        let(:os) { "mswin" }
+        it "bypasses the readability check" do
+          Conjur::Authn.send :fail_if_world_readable, path
+        end
+      end
+      context "on Linux" do
+        let(:os) { "linux" }
+        it "raises an error if the file is world readable" do
+          expect(File).to receive(:world_readable?).with(path).and_return(true)
+          expect { Conjur::Authn.send :fail_if_world_readable, path }.to raise_error("netrc (the-path) shouldn't be world-readable")
+        end
       end
     end
 
-    context "without specified netrc_path" do
-      let(:path) { nil }
-      it "uses default netrc path" do
-        expect(Netrc).to receive(:read).with(no_args).and_return netrc = double("netrc")
-        expect(Conjur::Authn.netrc).to eq(netrc)
+    context "loading" do
+      let(:netrc) { nil }
+      before do
+        allow(Conjur::Config).to receive(:[]).with(:netrc_path).and_return path
+      end
+      
+      context "with specified netrc_path" do
+        let(:path) { "/a/dummy/netrc/path" }
+        it "consults Conjur::Config for netrc_path" do
+          expect(Netrc).to receive(:read).with(path).and_return netrc = double("netrc")
+          expect(Conjur::Authn.netrc).to eq(netrc)
+        end
+      end
+  
+      context "without specified netrc_path" do
+        let(:path) { nil }
+        it "uses default netrc path" do
+          expect(Netrc).to receive(:read).with(no_args).and_return netrc = double("netrc")
+          expect(Conjur::Authn.netrc).to eq(netrc)
+        end
       end
     end
   end
