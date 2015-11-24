65e80ec8c372de7696fbd56b001d285bd9be87e9
Dustin Collins <dustinrcollins@gmail.com>
2015-11-24 16:33:12 -0500

    [#108895330] conjur script execute now takes --collection option

--- lib/conjur/command/script.rb
+++ lib/conjur/command/script.rb
@@ -18,8 +18,27 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+require 'conjur/command/dsl_command'
+
+require 'etc'
+require 'socket'
 
 class Conjur::Command::Script < Conjur::DSLCommand
+  class << self
+    def default_collection_user
+      # More accurate than Etc.getlogin
+      Etc.getpwuid(Process.uid).name
+    end
+
+    def default_collection_hostname
+      Socket.gethostname
+    end
+
+    def default_collection_name
+      [ default_collection_user, default_collection_hostname ].join('@')
+    end
+  end
+
   desc "Execute Conjur DSL scripts"
   command :script do |script|
     script.desc "Run a Conjur DSL script"
@@ -27,12 +46,24 @@ class Conjur::Command::Script < Conjur::DSLCommand
     script.command :execute do |c|
       acting_as_option(c)
 
+      c.desc "Script collection (target environment)"
+      c.arg_name "collection"
+      c.default_value default_collection_name
+      c.flag [:collection]
+
+
       c.desc "Load context from this config file, and save it when finished. The file permissions will be 0600 by default."
       c.arg_name "FILE"
       c.flag [:c, :context]
 
       c.action do |global_options,options,args|
-        run_script args, options
+        collection = options[:collection] || default_collection_name
+
+        run_script args, options do |runner, &block|
+          runner.scope collection do
+            block.call
+          end
+        end
       end
     end
   end
