d6625dfac32a90edf6671832e0c23a4becfd53c0
Kevin Gilpin <kgilpin@gmail.com>
2016-01-23 11:34:38 -0500

    run acceptance tests as part of build-deb test phase

--- ci/test.sh
+++ ci/test.sh
@@ -2,4 +2,17 @@
 
 conjur_cid="$1"
 
-docker exec $conjur_cid bash -c "cd /src/cli; unset CONJUR_AUTHN_LOGIN; bundle exec rake jenkins || true"
+cat << TEST | docker exec -i $conjur_cid bash
+#!/bin/bash -ex
+
+cd /src/cli
+
+unset CONJUR_AUTHN_LOGIN
+
+bundle exec rake jenkins || true
+bundle exec cucumber -r acceptance-features/support \
+	-r acceptance-features/step_definitions \
+	-f pretty \
+	-f junit --out acceptance-features/reports \
+	acceptance-features || true
+TEST
