ce1164f939276992106459aa5ec7c18797098559
Dustin Collins <dustinrcollins@gmail.com>
2016-01-29 10:32:05 -0500

    conjur host rotate_api_key

--- spec/command/hosts_spec.rb
+++ spec/command/hosts_spec.rb
@@ -69,4 +69,26 @@ describe Conjur::Command::Hosts, logged_in: true do
       end
     end
   end
+
+  context 'rotating api key' do
+    describe_command 'host rotate_api_key --host redis001' do
+      before do
+        expect(RestClient::Request).to receive(:execute).with({
+          method: :head,
+          url: 'https://core.example.com/hosts/redis001',
+          headers: {}
+        }).and_return true
+        expect(RestClient::Request).to receive(:execute).with({
+            method: :put,
+            url: 'https://authn.example.com/users/api_key?id=host%2Fredis001',
+            headers: {},
+            payload: ''
+        }).and_return double(:response, body: 'new api key')
+      end
+
+      it 'puts with basic auth' do
+        invoke
+      end
+    end
+  end
 end
