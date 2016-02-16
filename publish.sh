427248bb6a526271755e47780043a4f1d1919da3
Dustin Collins <dustinrcollins@gmail.com>
2016-02-16 15:34:48 -0500

    Fix typo in publish script

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ export DEBUG=true
 export GLI_DEBUG=true 
 
 DISTRIBUTION=$1
-COMPONENT=${2:-`echo $GIT_BRANCH | sed 's/^origin\///' | tr '/' '.'`)
+COMPONENT=${2:-`echo $GIT_BRANCH | sed 's/^origin\///' | tr '/' '.'`}
 
 debify publish --component $COMPONENT $DISTRIBUTION cli
