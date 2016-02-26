c3a7963fcc0e6310c386f6aef84a3942c1125ab1
Kevin Gilpin <kgilpin@gmail.com>
2016-02-26 15:03:51 -0500

    fix spec

--- spec/command/host_factories_spec.rb
+++ spec/command/host_factories_spec.rb
@@ -3,12 +3,15 @@ require 'conjur/command/host_factories'
 
 describe Conjur::Command::HostFactories, :logged_in => true do
   let (:group_memberships) { double(:group_memberships, :roleid => 'the-account:group:security_admin') }
-  let (:group) { double(:group, :exists? => true, :memberships => [group_memberships]) }
+  let (:current_role) { double(:current_role, :memberships => [ double(:current_role_role, roleid: 'the-account:user:dknuth') ]) }
+  let (:group_members) { double(:layer_members, :member => double(:member, :roleid => 'the-account:user:dknuth'), :admin_option => true ) }
+  let (:group) { double(:group, :exists? => true, :memberships => [group_memberships], :members => [group_members]) }
   let (:layer_members) { double(:layer_members, :member => double(:member, :roleid => 'the-account:group:security_admin'), :admin_option => true ) }
   let (:layer_role) { double(:layer_role, :members => [layer_members]) }
   let (:layer) { double(:layer, :exists? => true, :role => layer_role) }
 
   before do
+    allow(Conjur::Command.api).to receive(:role).with("user:dknuth").and_return current_role
     allow(Conjur::Command.api).to receive(:role).with("the-account:group:the-group").and_return group
     allow(Conjur::Command.api).to receive(:layer).with("layer1").and_return layer
   end
