3f1064c8e10943830e11f21f51d481e60c732a40
Dustin Collins <dustinrcollins@gmail.com>
2016-05-20 11:30:41 -0400

    Working implementation against API

--- lib/conjur/command/ldapsync.rb
+++ lib/conjur/command/ldapsync.rb
@@ -5,18 +5,29 @@ class Conjur::Command::LDAPSync < Conjur::Command
   command :'ldap-sync' do |cmd|
     cmd.desc 'LDAP Sync profile to use (defined in UI)'
     cmd.default_value 'default'
-    cmd.flag ['profile']
+    cmd.flag ['p', 'profile']
 
     cmd.desc 'Print the actions that would be performed'
     cmd.default_value false
     cmd.switch ['dry-run']
 
-    cmd.desc 'Output format of --dry-run mode (text, yaml)'
+    cmd.desc 'Output format of sync operation (text, yaml)'
     cmd.default_value 'text'
-    cmd.flag ['format']
+    cmd.flag ['f', 'format'], :must_match => ['text', 'yaml']
 
     cmd.action do |_ ,options, _|
+      format = options[:format] == 'text' ? 'application/json' : 'text/yaml'
       puts options
+
+      response = api.ldap_sync_now(options[:profile], format, options[:'dry-run'])
+
+      if options[:format] == 'text'
+        response['result']['actions'].each do |action|
+          puts action
+        end
+      else
+        puts YAML.dump(response)
+      end
     end
   end
 end
--- spec/command/ldapsync_spec.rb
+++ spec/command/ldapsync_spec.rb
@@ -1,9 +1,31 @@
 require 'spec_helper'
 
 describe Conjur::Command::LDAPSync, logged_in: true do
-  describe_command 'ldap-sync --dry-run' do
+  let(:json_response) { {
+      'result' => {
+          'actions' => [
+              "user 'Guest'", "group 'Domain Computers'"
+          ]
+      }
+  } }
+  let(:yaml_response) { [
+      'annotations' => {
+          'ldap-sync/source' => '192.168.99.100:389',
+          'ldap-sync/upstream-dn' => 'cn=Guest,dc=example,dc=org',
+      }
+  ].to_yaml }
+
+  describe_command 'ldap-sync -f text' do
     it 'Prints out actions to be taken in text' do
-      expect { invoke }.to write('{:"dry-run"=>true, :profile=>"default", :format=>"text"}')
+      expect_any_instance_of(Conjur::API).to receive(:ldap_sync_now).and_return json_response
+      expect { invoke }.to write("user 'Guest'\ngroup 'Domain Computers'")
+    end
+  end
+
+  describe_command 'ldap-sync -f yaml' do
+    it 'Prints out actions to be taken in text' do
+      expect_any_instance_of(Conjur::API).to receive(:ldap_sync_now).and_return yaml_response
+      expect { invoke }.to write(YAML.dump(yaml_response))
     end
   end
 end
