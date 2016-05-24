4b847c4dc5cf22987d9264b9ffa93f2ee8afa5e5
Kevin Gilpin <kgilpin@gmail.com>
2016-05-24 14:06:39 -0400

    update ldap-sync expectations

--- spec/command/ldapsync_spec.rb
+++ spec/command/ldapsync_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe Conjur::Command::LDAPSync, logged_in: true do
+  let(:timestamp) { Time.now.to_s }
   let(:json_response) { {
+      'events' => [
+        { "timestamp" => timestamp,
+          "severity" => "info",
+          "message" => "Performing sync"
+        }
+      ],
       'result' => {
-          'actions' => [
-              "user 'Guest'", "group 'Domain Computers'"
-          ]
+        'actions' => [
+            "user 'Guest'", "group 'Domain Computers'"
+        ]
       }
   } }
   let(:yaml_response) { [
@@ -15,17 +22,22 @@ describe Conjur::Command::LDAPSync, logged_in: true do
       }
   ].to_yaml }
 
-  describe_command 'ldap-sync -f text' do
-    it 'Prints out actions to be taken in text' do
+  describe_command 'ldap-sync now -f text' do
+    before {
       expect_any_instance_of(Conjur::API).to receive(:ldap_sync_now).and_return json_response
+    }
+    it 'prints out diagnostic events' do
+      expect { invoke }.to write([ timestamp, "info", "Performing sync" ].join("\t"))
+    end
+    it 'prints out actions as text' do
       expect { invoke }.to write("user 'Guest'\ngroup 'Domain Computers'")
     end
   end
 
-  describe_command 'ldap-sync -f yaml' do
-    it 'Prints out actions to be taken in text' do
+  describe_command 'ldap-sync now -f yaml' do
+    it 'prints out actions as unparsed yaml' do
       expect_any_instance_of(Conjur::API).to receive(:ldap_sync_now).and_return yaml_response
-      expect { invoke }.to write(YAML.dump(yaml_response))
+      expect { invoke }.to write(yaml_response)
     end
   end
 end
