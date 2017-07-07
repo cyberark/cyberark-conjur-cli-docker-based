9f97b50f0652e351d6ae6b52f7f665b580c2f9c2
Alan Potter <alan.potter@conjur.net>
2017-07-07 15:50:51 -0400

    reorg test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,38 @@
 #!/bin/bash -ex
 
-set -a
-
 : ${RUBY_VERSION=2.2}
-sed "s/\${RUBY_VERSION}/$RUBY_VERSION/" Dockerfile > Dockerfile.$RUBY_VERSION
-docker-compose build --pull
 
-function finish {
-  docker-compose down
+main() {
+  build
+  
+  start_possum
+  
+  run_tests
 }
-trap finish EXIT
 
-docker-compose pull pg possum 
-POSSUM_DATA_KEY="$(docker-compose run -T --no-deps possum data-key generate)"
+# internal functions
 
-docker-compose up -d possum
+build() {
+  # we can get rid of this once we upgrade to docker 17.06+
+  sed "s/\${RUBY_VERSION}/$RUBY_VERSION/" Dockerfile > Dockerfile.$RUBY_VERSION
+  
+  docker-compose build --pull
+}
 
-docker-compose run test ci/wait_for_server.sh
+start_possum() {
 
-CONJUR_AUTHN_API_KEY=$(docker-compose exec -T possum rails r "print Credentials['cucumber:user:admin'].api_key")
+  docker-compose pull pg possum 
+  
+  env POSSUM_DATA_KEY="$(docker-compose run -T --no-deps possum data-key generate)" \
+    docker-compose up -d possum
+  trap "docker-compose down" EXIT
+  
+  docker-compose run test ci/wait_for_server.sh
+}
 
-docker-compose run test "$@"
+run_tests() {
+  env CONJUR_AUTHN_API_KEY=$(docker-compose exec -T possum rails r "print Credentials['cucumber:user:admin'].api_key") \
+    docker-compose run test "$@"
+}
 
+main
