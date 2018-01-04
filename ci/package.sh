a93de8629735964cbd6c3d86d8858571d58bb04b
Dustin Collins <dustin.collins@cyberark.com>
2018-01-04 11:00:52 -0600

    stop appending timestamps to deb packages, not needed

--- ci/package.sh
+++ ci/package.sh
@@ -9,9 +9,7 @@ rake build
 
 gem install --no-ri --no-rdoc --install-dir /tmp/gems pkg/*.gem
 
-ITERATION=$(date +%s)
-
 find /tmp/gems/cache -name '*.gem' | xargs -rn1 \
-fpm --prefix $(gem environment gemdir) --iteration $ITERATION -s gem -t deb
+fpm --prefix $(gem environment gemdir) -s gem -t deb
 
 cp -a *.deb /share
