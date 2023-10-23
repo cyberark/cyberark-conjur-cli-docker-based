c53b8f89ee7933813550ca2438fbd6dc8373b94a
Bunny Remy <podmonkeys@gmail.com>
2023-10-23 10:15:40 -0400

    CONJSE-1802: Changes to publish.sh for ruby CVE-2023-5129

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-# conjurinc/publish-rubygem -> conjurinc/release-tools
-docker pull registry.tld/conjurinc/release-tools
+# conjurinc/publish-rubygem -> conjurinc/release-tools/bin/publish-rubygem
+docker pull registry.tld/conjurinc/release-tools/bin/publish-rubygem
 
 summon --yaml "RUBYGEMS_API_KEY: !var rubygems/api-key" \
   docker run --rm --env-file @SUMMONENVFILE -v "$(pwd)":/opt/src \
-  registry.tld/conjurinc/release-tools conjur-cli
+  registry.tld/conjurinc/release-tools/bin/publish-rubygem conjur-cli
